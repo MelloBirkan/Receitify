541a3f842c674e477680f5bf052769f4d8af9e3e
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-03-15 23:00:24 -0300

    Initial Commit

---
+++ Receitify/ReceitifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceitifyApp.swift
+//  Receitify
+//
+//  Created by Marcello Gonzatto Birkan on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ReceitifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
